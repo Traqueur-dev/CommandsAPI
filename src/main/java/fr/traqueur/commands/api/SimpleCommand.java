199daecc91bfe4dd5f992152fb6a8836d3d04007
Traqueur-dev <traqueur.dev@gmail.com>
2024-07-24 17:55:51 +0200

    feat (Command) : add SimpleCommand for common and basic usage with no generic type

---
+++ src/main/java/fr/traqueur/commands/api/SimpleCommand.java
@@ -0,0 +1,16 @@
+package fr.traqueur.commands.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class SimpleCommand extends Command<JavaPlugin> {
+
+    /**
+     * The constructor of the command.
+     *
+     * @param plugin The plugin that owns the command.
+     * @param name   The name of the command.
+     */
+    public SimpleCommand(JavaPlugin plugin, String name) {
+        super(plugin, name);
+    }
+}
