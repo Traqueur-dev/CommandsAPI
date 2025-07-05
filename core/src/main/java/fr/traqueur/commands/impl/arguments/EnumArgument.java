5cfbd6e58aa66ed2aed574415efb8384c8b5ad38
Traqueur <54551467+Traqueur-dev@users.noreply.github.com>
2025-07-05 11:12:08 +0200

    feat(converter): add argument converter for enum (#34)

---
+++ core/src/main/java/fr/traqueur/commands/impl/arguments/EnumArgument.java
@@ -0,0 +1,37 @@
+package fr.traqueur.commands.impl.arguments;
+
+import fr.traqueur.commands.api.arguments.ArgumentConverter;
+import fr.traqueur.commands.api.arguments.TabCompleter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EnumArgument<T extends Enum<T>, S> implements ArgumentConverter<Enum<T>>, TabCompleter<S> {
+
+    public static <E extends Enum<E>, S> EnumArgument<E, S> of(Class<E> enumClass) {
+        return new EnumArgument<>(enumClass);
+    }
+
+    private final Class<T> clazz;
+
+    public EnumArgument(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public Enum<T> apply(String s) {
+        try {
+            return Enum.valueOf(clazz, s);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<String> onCompletion(S sender, List<String> args) {
+        return Arrays.stream(clazz.getEnumConstants())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+}
