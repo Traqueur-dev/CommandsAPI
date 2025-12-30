be437b48e8ce33776dce3ea5749fc6cbf15943a3
Traqueur_ <traqueur.dev@gmail.com>
2025-12-30 16:52:49 +0100

    feat: add little cache system for player in arguments

--- spigot/src/main/java/fr/traqueur/commands/spigot/arguments/OfflinePlayerArgument.java
+++ spigot/src/main/java/fr/traqueur/commands/spigot/arguments/OfflinePlayerArgument.java
@@ -1,30 +1,46 @@
 package fr.traqueur.commands.spigot.arguments;
 
 import fr.traqueur.commands.api.arguments.ArgumentConverter;
+import fr.traqueur.commands.api.arguments.TabCompleter;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
  * Argument used to parse an {@link OfflinePlayer} from a string.
  */
-public class OfflinePlayerArgument implements ArgumentConverter<OfflinePlayer>, fr.traqueur.commands.api.arguments.TabCompleter<CommandSender> {
+public class OfflinePlayerArgument implements ArgumentConverter<OfflinePlayer>, TabCompleter<CommandSender> {
+
+    /**
+     * Cache TTL in milliseconds (longer for offline players as it's more expensive).
+     */
+    private static final long CACHE_TTL_MS = 5000;
+
+    /**
+     * Cached offline player names.
+     */
+    private volatile List<String> cachedNames = Collections.emptyList();
+
+    /**
+     * Last cache update time.
+     */
+    private volatile long cacheTime = 0;
 
     /**
      * Creates a new OfflinePlayerArgument.
      */
     public OfflinePlayerArgument() {}
 
-    @SuppressWarnings(value = "deprecation")
     /**
      * {@inheritDoc}
-     * <p>
-     * This implementation uses {@link Bukkit#getOfflinePlayer(String)} to parse the {@link OfflinePlayer}.
      */
+    @SuppressWarnings("deprecation")
     @Override
     public OfflinePlayer apply(String input) {
         return input != null ? Bukkit.getOfflinePlayer(input) : null;
@@ -32,10 +48,18 @@ public class OfflinePlayerArgument implements ArgumentConverter<OfflinePlayer>, 
 
     /**
      * {@inheritDoc}
-     * This implementation returns a list of all player names.
+     * Returns cached list of offline player names, refreshed every 5 seconds.
      */
     @Override
     public List<String> onCompletion(CommandSender sender, List<String> args) {
-        return Arrays.stream(Bukkit.getServer().getOfflinePlayers()).map(OfflinePlayer::getName).collect(Collectors.toList());
+        long now = System.currentTimeMillis();
+        if (now - cacheTime > CACHE_TTL_MS) {
+            cachedNames = Arrays.stream(Bukkit.getServer().getOfflinePlayers())
+                    .map(OfflinePlayer::getName)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+            cacheTime = now;
+        }
+        return cachedNames;
     }
 }
